bd52cec27d439840ba44e737e8b41aea699d28ba
Victor Kerdchuen <honeytose@gmail.com>
2023-04-21 15:59:02 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+    $("#dp-1").click(function(){
+        $("#dp-1-1").slideToggle(400);
+    });
+
+    $("#login").click(function(){
+        $(".login").fadeToggle(400);
+    });
+    
+
+});
+
+
+            
+        
